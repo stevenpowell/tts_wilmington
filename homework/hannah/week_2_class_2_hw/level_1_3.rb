4da9596bb7b4f883be3d7d88264a360ec4b9526b
Hannah C. Knuth <hcriser@bellsouth.net>
2016-12-16 17:32:57 -0500

    Finished Level 1 of Week 2 Class hw

---
+++ homework/hannah/week_2_class_2_hw/level_1_3.rb
@@ -0,0 +1,28 @@
+def get_number
+	puts "Enter a number:"
+	return gets.to_f
+end
+
+def fill_array(num)
+	count = 0
+	array = []
+	until count == num
+		array.push(get_number)
+		count += 1
+	end
+	return array
+end
+
+puts "How many number pairs would you like to add?"
+num_length = gets.chomp.to_i
+
+puts "Enter numbers for the first parameters at a time:"
+array1 = fill_array(num_length)
+puts "Enter numbers for the second parameters at a time:"
+array2 = fill_array(num_length)
+
+count = 0
+while count < num_length
+	puts array1[count].to_s + " + " + array2[count].to_s + " = " + (array1[count] + array2[count]).to_s
+	count += 1
+end
