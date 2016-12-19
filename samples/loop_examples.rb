91b9cfca32d5e2aa6baf3546b53ecd2966825cf9
Brian Triplett <brian.triplett@ncino.com>
2016-12-19 18:34:09 -0500

    Added looping example

---
+++ samples/loop_examples.rb
@@ -0,0 +1,39 @@
+# Another great example is at http://www.tutorialspoint.com/ruby/ruby_loops.htm
+
+size = 10
+
+size.times do
+
+	#do some things 'size' times
+
+end
+
+my_array = [1,2,3,4]
+
+my_array.each do |x|
+
+	# go over each item in my_array
+	# and let 'x' be the current item
+
+end
+
+array_1 = [1,2,3]
+array_2 = [4,5,6]
+
+for i in 0...array_1.length
+
+	# 'i' is just a counter but you can use it to access different arrays
+	# or keep track of how many times you looped
+
+	#sum.push(array_1[i] + array_2[i])
+
+end
+
+count = 0
+max = 10
+while count < max #also could use an 'until'
+
+	# do something while (or until a contition is true)
+	# does not neceesarily need to be counting
+
+end
