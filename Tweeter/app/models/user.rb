0c0ee4b2407e13c3929da06fb9e73158f62a407e
stevenpowell <stevenpowellmail@gmail.com>
2017-02-05 00:32:30 -0500

    adding tweeter

--- Tweeter/app/models/user.rb
+++ Tweeter/app/models/user.rb
@@ -4,8 +4,19 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-#Adding this per paperclip instruction
-  has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
-  validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
-#END
+# #Adding this per paperclip instruction
+#   has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+#   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
+# #END
+ has_many :tweets
+ has_many :relationships
+ has_many :friends, through: :relationships
+
+validates :username, presence: true
+
+
+  validates :name, uniqueness: true
+
+
+
 end
--- Tweeter/config/routes.rb
+++ Tweeter/config/routes.rb
@@ -1,5 +1,15 @@
 Rails.application.routes.draw do
+  get 'profiles/show'
+
+  get 'profiles/index'
+
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :tweets
+
+  root 'tweets#index'
+  
+  get "profiles/:id" => "profiles#show", as: :profile
+
+ get "profiles" => "profiles#index"
 end
